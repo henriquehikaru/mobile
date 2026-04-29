40a1669b1e23ece6d42caf142b23edc54933d290
henrique.hikaru <henrique.hikaru@unesp.br>
2026-04-29 20:22:42 -0300

    Create Button.tsx

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,6 +1,7 @@
 import { Text, View, StyleSheet, TouchableOpacity } from "react-native";
 import { Image } from 'expo-image';
 import { Link } from 'expo-router';
+import Button from "@/components/Button";
 import ImageViewer from "@/components/ImageViewer";
 
 const PlaceholderImage = require('@/assets/images/logo.png');
@@ -10,6 +11,11 @@ export default function Index() {
     <View style={styles.container}>
       <View style={styles.imageContainer}>
         <ImageViewer imgSource={PlaceholderImage} />
+      </View>
+
+      <View style={styles.footerContainer}>
+        <Button theme="primary" label="Choose a photo" />
+        <Button label="Use this photo" />
       </View>
 
       <Link href="/toDoList" asChild>
@@ -82,5 +88,10 @@ const styles = StyleSheet.create({
     color: '#fff',
     fontSize: 16,
     fontWeight: '600',
-  }
+  },
+
+  footerContainer: {
+    flex: 1 / 3,
+    alignItems: 'center',
+  },
 });
