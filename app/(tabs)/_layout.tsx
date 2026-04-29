e3b738f7e423a8b67fa2b0b799142a85df4c9995
henrique.hikaru <henrique.hikaru@unesp.br>
2026-04-29 19:51:26 -0300

    layout.

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,13 +1,3 @@
-<<<<<<< HEAD
-import { Stack } from 'expo-router';
-
-export default function RootLayout() {
-  return (
-    <Stack>
-      <Stack.Screen name="(tabs)" options={{ headerShown: false }}/>
-      <Stack.Screen name="+not-found" />
-    </Stack>
-=======
 import { Tabs } from 'expo-router';
 
 export default function RootLayout() {
@@ -16,6 +6,5 @@ export default function RootLayout() {
       <Tabs.Screen name="index" options={{ title: 'Home' }}/>
       <Tabs.Screen name="about" options={{ title: 'About' }}/> 
     </Tabs>
->>>>>>> 5d679421bc4e50c1229e36baf6e47bf85a8b02a5
   );
 }
